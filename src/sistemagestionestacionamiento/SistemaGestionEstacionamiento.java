8d237430782b8105f87a8d68600cfa0bab9cf49a
PaoloESAN <186249117+PaoloESAN@users.noreply.github.com>
2025-04-27 15:24:15 -0500

    primer commit - ventana login

---
+++ src/sistemagestionestacionamiento/SistemaGestionEstacionamiento.java
@@ -0,0 +1,25 @@
+package sistemagestionestacionamiento;
+
+import com.formdev.flatlaf.FlatDarculaLaf;
+import forms.Login;
+import javax.swing.UIManager;
+
+public class SistemaGestionEstacionamiento {
+
+    public static void main(String[] args) {
+        ventana();
+    }
+    private static void ventana(){
+        try {
+            UIManager.setLookAndFeel( new FlatDarculaLaf() );
+        }catch( Exception ex ) {
+            System.err.println( "Failed to initialize LaF" );
+        }
+        UIManager.put( "Button.arc", 17 );
+        UIManager.put( "Component.arc", 17 );
+        UIManager.put( "ProgressBar.arc", 17 );
+        UIManager.put( "TextComponent.arc", 17 );
+        Login login = new Login();
+        login.setVisible(true);
+    }
+}
